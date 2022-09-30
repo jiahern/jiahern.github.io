5c468f64030f764cff3f49871f8f0e558b2a657d
jiahern <jiahern@student.unimelb.edu.au>
2022-09-30 19:29:47 +0800

    Higher Order Component section Done

--- frontend-react/src/container/About/About.jsx
+++ frontend-react/src/container/About/About.jsx
@@ -1,33 +1,18 @@
 import React, { useState, useEffect } from 'react';
 import { motion } from 'framer-motion';
-import { images } from '../../constants';
+import { AppWrap } from '../../wrapper';
 
 import './About.scss';
-
-const abouts = [
-  {
-    title: 'Front-end Development',
-    description: 'I am a good Front-end developer',
-    imgUrl: images.about01,
-  },
-  {
-    title: 'Back-end Development',
-    description: 'I am a good Back-end developer',
-    imgUrl: images.about02,
-  },
-  {
-    title: 'Fullstack Development',
-    description: 'I am a good Fullstack developer',
-    imgUrl: images.about03,
-  },
-  {
-    title: 'UI/UX',
-    description: 'I am a good UI/UX developer',
-    imgUrl: images.about04,
-  },
-];
+import { urlFor, client } from '../../client';
 
 const About = () => {
+  const [abouts, setAbouts] = useState([]);
+
+  useEffect(() => {
+    const query = '*[_type == "abouts"]';
+    client.fetch(query).then((data) => setAbouts(data));
+  }, []);
+
   return (
     <>
       <h2 className="head-text">
@@ -45,7 +30,7 @@ const About = () => {
             className="app__profile-item"
             key={about.title + index}
           >
-            <img src={about.imgUrl} alt={about.title} />
+            <img src={urlFor(about.imgUrl)} alt={about.title} />
             <h2 className="bold-text" style={{ marginTop: 20 }}>
               {about.title}
             </h2>
@@ -58,4 +43,4 @@ const About = () => {
     </>
   );
 };
-export default About;
+export default AppWrap(About, 'about');
--- frontend-react/src/container/Header/Header.jsx
+++ frontend-react/src/container/Header/Header.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { motion } from 'framer-motion';
-
+import { AppWrap } from '../../wrapper';
 import { images } from '../../constants';
 import './Header.scss';
 
@@ -16,7 +16,7 @@ const scaleVariants = {
 };
 const Header = () => {
   return (
-    <div id="home" className="app__header app__flex">
+    <div className="app__header app__flex">
       <motion.div
         whileInView={{ x: [-100, 0], opacity: [0, 1] }}
         transition={{ duration: 0.5 }}
@@ -46,7 +46,7 @@ const Header = () => {
         <img src={images.profile} alt="profile_bg" />
 
         <motion.img
-          whileInView={{ opacity: [0, 1] }}
+          whileInView={{ scale: [0, 1] }}
           transition={{ duration: 1, ease: 'easeInOut' }}
           src={images.circle}
           alt="profile_circle"
@@ -69,4 +69,4 @@ const Header = () => {
   );
 };
 
-export default Header;
+export default AppWrap(Header, 'home');
